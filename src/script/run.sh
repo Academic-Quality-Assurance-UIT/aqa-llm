44d24d0bee3bd3519bad6e86081ebcc016742529
Hoang Hy <hoanghy01122003@gmail.com>
2025-07-11 07:26:23 +0700

    update

--- src/script/run.sh
+++ src/script/run.sh
@@ -1,5 +1,18 @@
 curl -fsSL https://ollama.com/install.sh | sh
 
+ollama serve
+
 ollama create aqa-sql -f ./Modelfile
 
 ollama run aqa-sql --keepalive=10h
+
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
+
+  ngrok config add-authtoken TOKEN
+
+  ngrok http http://localhost:11434
